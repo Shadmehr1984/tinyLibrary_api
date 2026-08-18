545796b06f4e659da122d270b1fe3ccf6dd9389e
shadmehr <ssszzzast@gmail.com>
2026-08-19 04:13:40 +0430

    so

--- database/seeders/LibrarianSeeder.php
+++ database/seeders/LibrarianSeeder.php
@@ -16,11 +16,11 @@ class LibrarianSeeder extends Seeder
     {
         //special librarian
         $librarian = new Librarian();
-        $librarian->name = $_ENV['SPECIAL_LIBRARIAN_NAME'];
-        $librarian->email = $_ENV['SPECIAL_LIBRARAIN_EMAIL'];
-        $librarian->password = Hash::make($_ENV['SPECIAL_LIBRARIAN_PASSWORD']);
-        $librarian->phone = $_ENV['SPECIAL_LIBRARIAN_PHONE'];
-        $librarian->address = $_ENV['SPECIAL_LIBRARIAN_ADDRESS'];
+        $librarian->name = 'shady';
+        $librarian->email = 'ssszzzast@gmail.com';
+        $librarian->password = Hash::make('ssszzzast21');
+        $librarian->phone = '+989389262193';
+        $librarian->address = 'yek ja dar kiran';
         $librarian->save();
         //random librarian
         Librarian::factory(2)->create();
