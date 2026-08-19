4c4e7c379c687a1a37730682e78e36480dffc0c4
shadmehr <ssszzzast@gmail.com>
2026-08-19 05:46:05 +0430

    Feat: add librarian update feature

--- app/Services/LibrarianServices.php
+++ app/Services/LibrarianServices.php
@@ -9,6 +9,7 @@ use App\Domain\ValueObjects\Phone;
 use App\Http\Requests\LibrarianDeleteRequest;
 use App\Http\Requests\LibrarianRequest;
 use App\Http\Requests\LibrarianSearchRequest;
+use App\Http\Requests\LibrarianUpdateRequest;
 use App\Repositories\LibrarianRepository;
 use Illuminate\Support\Facades\Hash;
 
@@ -21,6 +22,14 @@ class LibrarianServices
         'address' => ['column' => 'address', 'operator' => '='],
     ];
 
+    const UPDATE_REQUEST_ATTRIBUTES = [
+        'name',
+        'email',
+        'password',
+        'phone',
+        'address'
+    ];
+
     private static function convert_request_to_entity(LibrarianRequest $request): Librarian
     {
         $builder = new LibrarianBuilder();
@@ -28,6 +37,23 @@ class LibrarianServices
         $entity = $builder->set_id(null)->set_address($request->address)->set_email(new Email($request->email))->set_name($request->name)->set_password(Hash::make($request->password))->set_phone(new Phone($request->phone))->build();
 
         return $entity;
+    }
+
+    private static function take_update_attributes(LibrarianUpdateRequest $request): array
+    {
+        $attributes = [];
+        
+        foreach (static::UPDATE_REQUEST_ATTRIBUTES as $attribute) {
+            if ($request->$attribute != null){
+                $attributes[$attribute] = $request->$attribute;
+            }
+        }
+
+        if (isset($attribute['password'])){
+            $attribute['password'] = Hash::make($attribute['password']);
+        }
+
+        return $attribute;
     }
 
     public static function add(LibrarianRequest $request)
@@ -47,6 +73,21 @@ class LibrarianServices
         $repository = new LibrarianRepository($entity);
 
         $repository->delete();
+    }
+
+    public static function update(LibrarianUpdateRequest $request)
+    {
+        $entity = LibrarianRepository::search([
+            ['email', '=', $request->email]
+        ])[0];
+
+        $repository = new LibrarianRepository($entity);
+
+        $attributes = static::take_update_attributes($request);
+
+        $repository->update($attributes);
+
+        $repository->save();
     }
 
     public static function search(LibrarianSearchRequest $request)
