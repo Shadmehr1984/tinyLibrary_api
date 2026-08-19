e5a4c9072537c38c9b7b7a03fb973bebc3377d20
shadmehr <ssszzzast@gmail.com>
2026-08-19 04:48:24 +0430

    Feat: add book update feature Note: not tested yet

--- app/Services/BookServices.php
+++ app/Services/BookServices.php
@@ -9,6 +9,7 @@ use App\Domain\ValueObjects\ISBN;
 use App\Http\Requests\BookDeleteRequest;
 use App\Http\Requests\BookRequest;
 use App\Http\Requests\BookSearchRequest;
+use App\Http\Requests\BookUpdateRequest;
 use App\Repositories\BookRepository;
 
 class BookServices
@@ -32,6 +33,18 @@ class BookServices
         'location' => ['column' => 'location', 'operator' => '='],
     ];
 
+    const UPDATE_REQUEST_ATTRIBUTES = [
+        'title',
+        'author',
+        'isbn',
+        'published',
+        'publisher',
+        'category_id',
+        'total_copies',
+        'description',
+        'location',
+    ];
+
     private static function convert_request_to_entity(BookRequest $request): Book
     {
         $builder = new BookBuilder();
@@ -40,6 +53,19 @@ class BookServices
             set_description($request->description)->set_location($request->location)->set_deleted_at(null)->build();
 
         return $entity;
+    }
+
+    private static function take_update_attributes(BookUpdateRequest $request): array
+    {
+        $attributes = [];
+        
+        foreach (static::UPDATE_REQUEST_ATTRIBUTES as $attribute) {
+            if ($request->$attribute != null){
+                $attributes[$attribute] = $request->$attribute;
+            }
+        }
+
+        return $attribute;
     }
 
     public static function add(BookRequest $request)
@@ -61,10 +87,23 @@ class BookServices
         $repository->save();
     }
 
+    public static function update(BookUpdateRequest $request)
+    {
+        $entity = BookRepository::search([['isbn', '=', $request->target_isbn->__toString()]])[0];
+
+        $repository = new BookRepository($entity);
+
+        $attributes = static::take_update_attributes($request);
+
+        $repository->update($attributes);
+
+        $repository->save();
+    }
+
     public static function search(BookSearchRequest $request)
     {
         $attributes = [];
-        
+
         foreach (static::SEARCH_REQUEST_ATTRIBUTES as $attribute => $search_detail) {
             if ($request->$attribute == null) continue;
             $attributes[] = [$search_detail['column'], $search_detail['operator'], $request->$attribute];
