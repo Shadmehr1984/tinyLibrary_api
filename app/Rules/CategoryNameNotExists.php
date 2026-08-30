83a51663b06925d3e4a1b4cc00bf6474be836a16
shadmehr <ssszzzast@gmail.com>
2026-08-30 06:43:30 +0430

    Feat: add CategoryNameNotExists rule

---
+++ app/Rules/CategoryNameNotExists.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Translation\PotentiallyTranslatedString;
+
+class CategoryNameNotExists implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  Closure(string, ?string=): PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $name = DB::table('categories')->select(['name'])->where('name', '=', $value)->get();
+
+        if ($name){
+            $fail('category '.$value.' already exists');
+        }
+    }
+}
