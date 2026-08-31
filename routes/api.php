e241c9199d48a794fede3ee532893de416bef95c
shadmehr <ssszzzast@gmail.com>
2026-09-01 01:42:10 +0430

    Test: test some routes

--- routes/api.php
+++ routes/api.php
@@ -64,23 +64,23 @@ Route::post('/test', function(LibrarianSearchRequest $request){
 Route::post('/v1/add_book', [LibrarianController::class, 'add_book'])->middleware('auth:librarian');
 
 Route::delete('/v1/delete_book', [LibrarianController::class, 'delete_book'])->middleware('auth:librarian');
-//!
+
 Route::post('/v1/search_book', [LibrarianController::class, 'search_book'])->middleware('auth:librarian');
-//!
+
 Route::put('/v1/update_book', [LibrarianController::class, 'update_book'])->middleware('auth:librarian');
 
 Route::post('/v1/add_member', [LibrarianController::class, 'add_member'])->middleware('auth:librarian');
 
 Route::put('/v1/deactivate_member', [LibrarianController::class, 'deactivate_member'])->middleware('auth:librarian');
-//!
+
 Route::put('/v1/activate_member', [LibrarianController::class, 'activate_member'])->middleware('auth:librarian');
-//!
+
 Route::delete('/v1/delete_member', [LibrarianController::class, 'delete_member'])->middleware('auth:librarian');
-//!
+
 Route::post('/v1/search_member', [LibrarianController::class, 'search_member'])->middleware('auth:librarian');
-//!
+
 Route::put('/v1/update_member', [LibrarianController::class, 'update_member'])->middleware('auth:librarian');
-//!
+
 Route::post('/v1/add_category', [LibrarianController::class, 'add_category'])->middleware('auth:librarian');
 //!
 Route::delete('/v1/delete_category', [LibrarianController::class, 'delete_category'])->middleware('auth:librarian');
